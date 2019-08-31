37cd52f230c9498d30c217dcbcd3e80d6302e3d9
omicheal <omichaeltmichael@live.co.uk>
2019-08-31 23:13:23 +0100

    html and js

---
+++ webapp/index.js
@@ -0,0 +1,45 @@
+const webcamElement = document.getElementById('webcam');
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  await setupWebcam();
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+
+    await tf.nextFrame();
+  }
+}
+
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
+
+app();
